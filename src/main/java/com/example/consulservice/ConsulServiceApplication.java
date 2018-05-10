75c9b885d5557f339cd1cafcf8bfbf97389b112c
Manuel Páez <m.paez589@gmail.com>
2018-05-10 15:08:58 +0200

    Add files via upload

---
+++ src/main/java/com/example/consulservice/ConsulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.consulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulServiceApplication.class, args);
+	}
+}
