8262bfed17dcfe86e7c6e1bdf1e1ecfdc94122c2
MANUEL ANGEL PAEZ <mapaez@externos.bankia.com>
2018-05-10 18:11:03 +0200

    Register with consul and also get properties

--- src/main/java/com/example/consulservice/ConsulServiceApplication.java
+++ src/main/java/com/example/consulservice/ConsulServiceApplication.java
@@ -1,12 +1,70 @@
 package com.example.consulservice;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsulServiceApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsulServiceApplication.class, args);
+	}
+	
+	@RestController
+	public class DiscoveryClientController {
+	  
+	    @Autowired
+	    private DiscoveryClient discoveryClient;
+	    
+	    @Autowired
+	    private MyProperties myProperties;
+	    
+	    @Value("${my.prop}")
+	    private String value;
+	 
+	    public Optional<URI> serviceUrl() {
+	        return discoveryClient.getInstances("consul-service")
+	          .stream()
+	          .map(si -> si.getUri())
+	          .findFirst();
+	    }
+	     
+	    @GetMapping("/ping")
+	    public String ping() {
+	    	
+	    	List<ServiceInstance> instances = discoveryClient.getInstances("consul-service");
+	    	for (ServiceInstance serviceInstance : instances) {
+				System.out.println(serviceInstance.toString());
+			}
+	        return "pong";
+	    }
+	    
+	    @GetMapping("/prop")
+	    public ResponseEntity<String> property() {
+	    	
+	    	return new ResponseEntity<String>(value, HttpStatus.OK);
+	    }
+	    
+	    // To refresh the value, send a POST to http://host:port/actuator/refresh
+	    @GetMapping("/propWithRefresh")
+	    public ResponseEntity<String> propertyRefreshable() {
+	    	
+	    	return new ResponseEntity<String>(myProperties.getProp(), HttpStatus.OK);
+	    }
 	}
 }
